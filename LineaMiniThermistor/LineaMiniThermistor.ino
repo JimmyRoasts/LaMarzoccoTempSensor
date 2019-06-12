faf927315c4b456af68578a61ea05582ab5aaa0f
James <James@williamstownroasters.com>
2019-06-12 15:40:03 +1000

    Update LineaMiniThermistor.ino

--- LineaMiniThermistor/LineaMiniThermistor.ino
+++ LineaMiniThermistor/LineaMiniThermistor.ino
@@ -22,3 +22,76 @@
 //scaling value to convert voltage
 
 #define VOLTAGEREF 4.585
+//reference voltage
+
+#define CLK 5 //Clock Line for LM1637
+#define DIO 4 //IO Line for LM1637
+
+
+TM1637Display display(CLK, DIO); 
+ 
+int samples[NUMSAMPLES];
+
+float sum = 0;                    // sum of samples taken
+unsigned char sample_count = 0; // current sample number
+float voltage = 0.0;            // calculated voltage
+ 
+void setup(void) {
+  display.setBrightness(0x07);
+  Serial.begin(9600);
+  analogReference(DEFAULT);
+}
+ 
+void loop(void) {
+  uint8_t i;
+  float average;
+
+    // take a number of analog samples and add them up
+    
+    while (sample_count < NUMSAMPLES) {
+        sum += (analogRead(THERMISTORPIN)*(VOLTAGEREF / 1023) * VOLTAGESCALE);
+        sample_count++;
+        delay(30);
+    }
+    
+    // calculate the voltage
+    // use 5.0 for a 5.0V ADC reference voltage
+    // 4.61V is the calibrated reference voltage
+    
+    voltage = ((float)sum / (float)NUMSAMPLES);
+    
+    // send voltage for display on Serial Monitor
+    // voltage multiplied by 12.1 when using voltage divider that
+    // divides by 12.1
+
+    Serial.print(voltage);
+    Serial.println (" V");
+    sample_count = 0;
+    sum = 0;
+  
+
+
+  average = SERIESRESISTOR / (voltage / 0.92); //0.92 is a scaling factor that the thermistor seems to line up with.
+  Serial.print("Thermistor resistance "); 
+  Serial.println(average);
+  
+  float steinhart;
+  steinhart = (SERIESRESISTOR / (VOLTAGEREF - voltage))/ THERMISTORNOMINAL;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert to C
+  steinhart /= 1.0;
+  
+  Serial.print("Temperature "); 
+  Serial.print(steinhart);
+  Serial.println(" *C");
+
+  int temp = steinhart*100;
+  float temp2 = temp/10;
+
+
+  display.showNumberDecEx(temp2*10,64,false);
+  
+}
